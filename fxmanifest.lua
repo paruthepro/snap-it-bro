6079adcf3fca33a6f1a2f2ac1d338ce0d886782a
ParuThePro <35810485+paruthepro@users.noreply.github.com>
2023-03-09 17:01:18 +0000

    Update fxmanifest.lua

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -4,7 +4,7 @@ lua54 'yes'
 
 author 'Paru'
 description 'Simple camera script designed in about 25 ish minutes'
-version '1.0.7'
+version '1.0'
 
 client_scripts 'client.lua'
 server_scripts 'server.lua'
