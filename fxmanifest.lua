5a356b3500d9084534df82f27114751e3b69184d
ParuThePro <35810485+paruthepro@users.noreply.github.com>
2023-03-09 17:01:05 +0000

    Added FXManifest.lua for conveinience

---
+++ fxmanifest.lua
@@ -0,0 +1,11 @@
+fx_version 'cerulean'
+game 'gta5'
+lua54 'yes'
+
+author 'Paru'
+description 'Simple camera script designed in about 25 ish minutes'
+version '1.0.7'
+
+client_scripts 'client.lua'
+server_scripts 'server.lua'
+shared_script 'config.lua'
